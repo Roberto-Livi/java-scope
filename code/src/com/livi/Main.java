e56825de908a13065e278f9fd98ff825067f7bd0
Roberto-Livi <robertomlivi@outlook.com>
2020-11-08 12:22:41 -0500

    demonstrating scope and visibility for varThree in the Main method

--- code/src/com/livi/Main.java
+++ code/src/com/livi/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         String varFour = "this is private to main()";
+
         ScopeCheck scopeInstance = new ScopeCheck();
+        scopeInstance.useInner();
         System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
-        System.out.println();
+        System.out.println(varFour);
 
         scopeInstance.timesTwo();
         System.out.println("***********************************");
