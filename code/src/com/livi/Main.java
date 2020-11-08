aca3053481d56d8968ebdb684980f4b2beda79f5
Roberto-Livi <robertomlivi@outlook.com>
2020-11-08 12:14:23 -0500

    refactored the variables of the ScopeCheck and inner class to demonstrate the scope visibility for each one of those variables

--- code/src/com/livi/Main.java
+++ code/src/com/livi/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String varFour = "this is private to main()";
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+        System.out.println();
+
+        scopeInstance.timesTwo();
+        System.out.println("***********************************");
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+
     }
 
 }
