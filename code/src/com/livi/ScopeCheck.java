fba6a12f20f7b4b36bdb6fb9a416f454dc09dc36
Roberto-Livi <robertomlivi@outlook.com>
2020-11-08 12:04:28 -0500

    added an InnerClass within the ScopeCheck class with its own timesTwo method

--- code/src/com/livi/ScopeCheck.java
+++ code/src/com/livi/ScopeCheck.java
@@ -13,4 +13,26 @@ public class ScopeCheck {
         return privateVar;
     }
 
+    public void timesTwo() {
+        int privateVar = 2;
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i + " times two is " + i * privateVar);
+        }
+    }
+
+    public class InnerClass {
+        public int privateVar = 3;
+
+        public InnerClass() {
+            System.out.println("InnerClass created, privateVar is " + privateVar);
+        }
+
+        public void timesTwo() {
+            ScopeCheck.this.timesTwo();
+            for(int i = 0; i < 10; i++) {
+                System.out.println(i + " times two is " + i * ScopeCheck.this.privateVar);
+            }
+        }
+    }
+
 }
